b33be6d1ffa768ad529522177fcb01633f033624
Suresh014 <vrsuresh014@gmail.com>
2025-04-30 04:51:43 +0530

    Datatypes

---
+++ Varaible.js
@@ -0,0 +1,48 @@
+
+var x = 20;
+
+var y = 30;
+
+z = x+y
+
+console.log(typeof(z))
+
+console.log("Swaran")
+
+
+let stuname='Shabari'
+
+console.log(stuname)
+
+console.log(typeof(stuname))
+
+
+const stuno=`sreehsri@321`
+
+console.log(stuno)
+
+console.log(typeof(stuno))
+
+
+{
+    var student="Arjun"
+
+
+    if (5>3){
+        console.log(student)
+
+        let num=1234
+
+        console.log(num)
+    }
+
+
+    const num1=78920
+
+    console.log(num1)
+
+    console.log(typeof(num1))
+    
+    
+}
+

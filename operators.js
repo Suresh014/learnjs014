b4346df754daf8fcc871ed9414b115ebbdcaae23
Suresh014 <vrsuresh014@gmail.com>
2025-04-30 14:23:18 +0530

    newfile

--- operators.js
+++ operators.js
@@ -1,2 +1,294 @@
+//Arithmetic
+
+//Adding
+
+x=20
+y=30
+z=x+y
+console.log("Adding two numbers :",z)
+
+
+let a=1019
+let b=2015
+let c =  a + b
+console.log("Adding sum of two numbers :",c)
+
+
+const e = "Ayodhya was famous place by "
+const d = "ram mandir"
+const f = e + d
+console.log("One of the place is",f)
+
+
+//Substracting
+
+num = 500-98-87-76-65-54-43-32-21-"2025"-2026
+console.log("Value of num is:",num)
+
+
+var ad = 98*40
+var bd = 56*32
+var dd = ad - bd
+console.log("substract of two numbers dd is:",dd)
+
+
+let aa = 986+623
+let bb = 378+487
+let cd = aa-bb
+console.log("sub of two numbers cd is:",cd)
+
+
+const ee = (65*75)
+const ff = (450+540)
+const gg = ee-ff
+console.log("sub of two numbers gg is:",gg)
+
+
+//Multipling
+
+ let g = 125
+let h = 25
+let i = g * h
+console.log("Multiple of two numbers :",i)
+
+
+const j = 23+49
+const k = 35+98
+const l = j*k
+console.log("Multiple of two numbers :",l)
+
+
+var m = 145-75
+var n = 320-165
+var p = m*n
+console.log("Multiple of two numbers :",p)
+
+q = 23*45*98*45*"2025"*2026
+console.log("Value of q is:",q)
+
+
+//Dividing
+
+var ab = 98
+var ba = 15
+var ca = ab/ba
+console.log("divid of two numbers ca is :",ca)
+
+
+let bc = (12*12)
+let cb = (11*9)
+let abc = bc/cb
+console.log("divid of two numbers abd is :",abc)
+
+
+const ae = (890+450)
+const af = (758+362)
+const ag = ae/af
+console.log("divid of two numbers ag is :",ag)
+
+
+//Remainder
+
+num1=98
+num2=42
+num3=num1%num2
+console.log("remainder value num3 is :",num3)
+
+
+const nu1=(980+15)
+const nu2=(420+15)
+const nu3=nu1%nu2
+console.log("remainder value nu3 is :",nu3)
+
+
+let fa = (15*45)
+let fb = (12*13)
+let afb = fa%fb
+console.log("remainder value afb is :",afb)
+
+
+var ah = (678-543)
+var ai = (454-345)
+var ahi = ah%ai
+console.log("remainder value ahi is :",ahi)
+
+//Incrementing
+
+bca = 786
+bca++
+console.log("value of bca is:",bca)
+
+let cab = (225+625)
+cab++
+console.log("value of cab is:",cab)
+
+/*const afc = 975
+
+afc++
+console.log("value of afc is:",afc)*/
+
+var aca = (15*35)
+aca++
+console.log("value of aca is:",aca)
+
+
+//Decrementing
+
+bac = 785
+bac--
+console.log("value of bac is:",bac)
+
+let cba = (235+625)
+cab--
+console.log("value of cba is:",cba)
+
+var aac = (15*45)
+aac--
+console.log("value of aac is:",aac)
+
+
+//Exponentiation
+
+var aae = 225
+aae**3
+console.log("value of aae is:",aae)
+
+let aab = (12*12)
+aab**2
+console.log("value of aab is:",aab)
+
+const aad = 45/15
+aad**4
+console.log("value is aad is:",aad)
+
+//Assignment
+
+caa = 25
+caa+=10
+console.log("value of caa is:",caa)
+
+bbc = 65
+cbb = 35
+bbc+=cbb //bbc=bbc+cbb
+console.log("value of bcc is:",bbc)
+
+
+aqq =35
+aqq-=10 //aqq=aqq-10
+console.log("value aqq is:",aqq)
+
+
+ass = 65
+ass*= 15
+console.log("value of ass is:",ass)
+
+
+app = 85
+app/=25
+console.log("value of app is:",app)
+
+
+aww = 48
+aww %= 5
+console.log("value of aww is:",aww)
+
+
+aee = 45
+aee **=3
+console.log("value of aee is:",aee)
+
+
+//comparision
+
+console.log(2=="2")
+
+console.log(3==="3")
+
+console.log(4===4)
+
+console.log(1023<1000)
+
+console.log(25>15)
+
+console.log(980>2025)
+
+console.log(5>=5)
+
+console.log(125<=135)
+
+console.log(3!=4)
+
+
+//Logical
+
+//AND
+console.log((2==2)&&(4>3))
+
+console.log((3<=2)&&(3<4))
+
+console.log((2=="2")&&(2===2))
+
+console.log((3!=4)&&(5<6))
+
+//OR
+console.log((2!=2)||(5<6))
+
+console.log((100<98)||(121>130))
+
+//NOT
+console.log(3!=4)
+
+console.log(!(4==4))
+
+//Ternary
+
+adc = "Sanjay"
+
+acd = "Ranaveer"
+
+dca = 4==4 ? adc:acd
+console.log(dca)
+
+
+Atm_pin_status = "Correct pin"
+
+Atm_pin_status1 = "Incorrect pin"
+
+Amount_withdraw = false
+
+Status_of_Amount = Amount_withdraw ? Atm_pin_status : Atm_pin_status1
+
+console.log(Status_of_Amount)
+
+
+//Coersion
+
+name = "Raju"+78
+
+console.log(name)
+
+console.log(20+45+67+"2025"+2026)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
